23b25313e0fbd1a72a689d1e1c4befd71988b987
Nicolas Pirmez <n.pirmez@ephec.be>
2025-03-19 11:24:49 +0100

    test

--- includes/otp.php
+++ includes/otp.php
@@ -2,12 +2,15 @@
 require_once __DIR__ . '/../vendor/autoload.php'; // Adjusted path to autoload.php
 
 use RobThree\Auth\TwoFactorAuth;
+use RobThree\Auth\Providers\Qr\BaconQrCodeProvider;
 
 class OTP {
     private $tfa;
 
     public function __construct() {
-        $this->tfa = new TwoFactorAuth('MyDressing');
+        // Ensure the QR code provider is correctly instantiated
+        $qrCodeProvider = new BaconQrCodeProvider();
+        $this->tfa = new TwoFactorAuth('MyDressing', 6, 30, 'sha1', $qrCodeProvider);
     }
 
     public function generateSecret() {
